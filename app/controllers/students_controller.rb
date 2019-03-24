314c3575f940aa904f17bf98e9660a9703c9b2ba
Katie Moosman <moosman.katie@gmail.com>
2019-03-24 21:12:36 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,9 @@ class StudentsController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+
+  def students
+    @students = Student.all
+
+  end
 end
