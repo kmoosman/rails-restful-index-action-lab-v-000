708abf9a6d18c86d7d020d8dc5748d41603a1f3e
Katie Moosman <moosman.katie@gmail.com>
2019-03-24 21:41:32 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,7 +3,7 @@ class StudentsController < ApplicationController
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
 
-  def students
+  def index
     @students = Student.all
   end
 end
