9991e5b53e53bcf6ed6bc848d73f9db69f82324a
Katie Moosman <moosman.katie@gmail.com>
2019-03-24 21:12:47 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  def students
-    @students = Student.all
-    get 'students', to: 'static#index'
-  end
+  get 'students', to: 'static#index'
+  
 
 end
